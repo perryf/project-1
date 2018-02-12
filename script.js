753b5d1e4b1d712a4ceeb7ceb3db71563c3d495e
Angela Malone <a.malone1193@gmail.com>
2018-02-12 11:45:48 -0500

    converted questions into an array of objects to be looped

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
-question: "Where was Kevin Love selected in the 2008 NBA Draft?"
+const quizQuestions = [
 
-one: "1st Round, 5th Pick"*
-two: "2nd Round, 32nd Pick"
-three: "1st Round, 19th Pick"
-four: "2nd Round, 48th Pick"
+{question: "Where was Kevin Love selected in the 2008 NBA Draft?",
 
-question: "Kevin Love was traded to the Cleveland Cavaliers from what team?"
+one: "1st Round, 5th Pick",/* */
+two: "2nd Round, 32nd Pick",
+three: "1st Round, 19th Pick",
+four: "2nd Round, 48th Pick"},
 
-one: "Chicago Bulls"
-two: "Memphis Grizzlies"*
-three: "Minnesota Timberwolves"
-four: "New York Knicks"
+{question: "Kevin Love was traded to the Cleveland Cavaliers from what team?",
 
-question: "What was Kevin's number before he switched to '0'?"
+one: "Chicago Bulls",
+two: "Memphis Grizzlies",/* */
+three: "Minnesota Timberwolves",
+four: "New York Knicks"},
 
-one: "17"
-two: "42"*
-three: "23"
-four: "56"
+{question: "What was Kevin's number before he switched to '0'?",
 
-question: "What NBA record did Kevin set during the November 23, 2016 game against the Portland Trailblazers?"
+one: "17",
+two: "42",/* */
+three: "23",
+four: "56"},
 
-one: "Most points scored in a first quarter"*
-two: "Most points scored in a game"
-three: "Most consecutive free throws in a game"
-four: "Most rebounds in a half"
+{question: "What NBA record did Kevin set during the November 23, 2016 game against the Portland Trailblazers?",
 
-question: "Which one of Kevin's Teammates came up with the unofficial team mascot, 'Lil Kev'?"
+one: "Most points scored in a first quarter",/* */
+two: "Most points scored in a game",
+three: "Most consecutive free throws in a game",
+four: "Most rebounds in a half"},
 
-one: "Lebron James"
-two: "Tristan Thompson"
-three: "Richard Jefferson"*
-four: "Matthew Dellevedova"
+{question: "Which one of Kevin's Teammates came up with the unofficial team mascot, 'Lil Kev'?",
 
-question: "What was Kevin's 3PT% in the 2016 NBA Playoffs?"
+one: "Lebron James",
+two: "Tristan Thompson",
+three: "Richard Jefferson",/* */
+four: "Matthew Dellevedova"},
 
-one: "36.7%"
-two: "41.4%"*
-three: "46.8%"
-four: "34.2%"
+{question: "What was Kevin's 3PT% in the 2016 NBA Playoffs?",
 
-question: "Kevin has been an NBA All-Star ____ times and has participated in ___ All-Star games"
+one: "36.7%",
+two: "41.4%",/* */
+three: "46.8%",
+four: "34.2%"},
 
-one: "four, four"
-two: "three, one"
-three: "three, two"
-four: "five, three"*
+{question: "Kevin has been an NBA All-Star ____ times and has participated in ___ All-Star games",
 
-question: "Kevin is currently a model for which clothing brand?"
+one: "four, four",
+two: "three, one",
+three: "three, two",
+four: "five, three"/* */},
 
-one: "J. Crew"
-two: "Gap"
-three: "Banana Republic"*
+{question: "Kevin is currently a model for which clothing brand?",
+
+one: "J. Crew",
+two: "Gap",
+three: "Banana Republic",/* */
 four: "AllSaints"
+}
+]
